909755f7b5f84710c2d6c523e2a8a86421a13eed
Majid Ataee <ataiemajid_63@hotmail.com>
2018-02-21 12:43:37 +0330

    Added converting methods for timestamp and julian day.

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,7 +3,60 @@ class Calendar {
 	constructor (timestamp, offset) {
 		this._timestamp = timestamp;
 		this._offset = offset;
+
+		this.J1970 = 2440587.5;			// Julian date at Unix epoch: 1970-01-01
+		this.DayInSecond = 86400;
 	}	
 
-	toJulianNumber () {}
+	timestampToJulianDay (timestamp) {
+		let julianDay = this.J1970 + timestamp / this.DayInSecond;
+
+		return julianDay;
+	}
+
+	julianDayToTimestamp (julianDay) {
+		let timestamp = Math.round((julianDay - this.J1970) * this.DayInSecond);
+		
+		return timestamp;
+    }
+
+	julianDayWithoutTime (julianDay) {
+		
+		return Math.floor(julianDay) + 0.5;
+	}
+
+	extractJulianDayTime (julianDay) {
+        julianDay += 0.5;
+
+        // Astronomical to civil
+        let time = ((julianDay - Math.floor(julianDay)) * this.DayInSecond) + 0.5;
+
+        return {
+        	"hour": Math.floor(time / 3600),
+        	"minute": Math.floor((time / 60) % 60),
+        	"second": Math.floor(time % 60)
+        };
+    }
+
+    addTimeToJulianDay (julianDay, hour, minute, second) {
+    	let timestamp = this.julianDayToTimestamp(julianDay);
+    	timestamp += (hour * 3600) + (minute * 60) + second;
+
+    	return this.timestampToJulianDay(timestamp);
+    }
+
+    mod (a, b) {
+    	return a - (b * Math.floor(a / b));
+    }
+
+    isLeap (year) {}
+
+    dateToJulianDay (year, month, day, hour, minute, second) {}
+
+    dateToTimestamp (year, month, day, hour, minute, second) {}
+
+    julianDayToDate (julianDay) {}
+
+    timestampToDate (timestamp) {}
+
 }
