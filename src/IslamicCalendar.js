a8c8b59989303731b3cf046ca1cef43db3ea62be
Majid Ataee <ataiemajid_63@hotmail.com>
2018-03-05 20:42:10 +0330

    Implemented islamic calendar methods.

--- src/IslamicCalendar.js
+++ src/IslamicCalendar.js
@@ -1,9 +1,48 @@
 class IslamicCalendar extends Calendar {
-	constructor () {
-		super();
+	
+	constructor (timestamp, offset) {
+		super(timestamp, offset);
+		
+		this.IslamicEpoch = 1948439.5;
+		Object.defineProperty(this, 'IslamicEpoch', {
+            writable: false,
+            configurable: false
+        });
 	}
 
-	toJulianNumber () {
-		
+	isLeap (year) {
+        let isLeap = (((year * 11) + 14) % 30) < 11;
+		return isLeap;
 	}
+
+	dateToJulianDay (year, month, day, hour, minute, second) {
+        let julianDay = day;
+
+        julianDay += Math.ceil((month - 1) * 29.5);
+        julianDay += (year - 1) * 354;
+        julianDay += Math.floor(((11 * year) + 3) / 30);
+        julianDay += this.IslamicEpoch - 1;
+
+		return this.addTimeToJulianDay(julianDay, hour, minute, second);
+	}
+
+    julianDayToDate (julianDay) {
+        let time = this.extractJulianDayTime(julianDay);
+        
+        julianDay = this.julianDayWithoutTime(julianDay);
+        julianDay = Math.floor(julianDay) + 0.5;
+        
+        let year = Math.floor((((julianDay - this.IslamicEpoch) * 30) + 10646) / 10631);
+        let month = Math.min(12, Math.ceil((julianDay - (29 + this.julianDayWithoutTime(this.dateToJulianDay(year, 1, 1, time.hour, time.minute, time.second)))) / 29.5) + 1);
+        let day = (julianDay - this.julianDayWithoutTime(this.dateToJulianDay(year, month, 1, time.hour, time.minute, time.second))) + 1;
+
+        return {
+        	"year": year,
+        	"month": month,
+        	"day": day,
+        	"hour": time.hour,
+        	"minute": time.minute,
+        	"second": time.second,
+        }
+    }
 }
