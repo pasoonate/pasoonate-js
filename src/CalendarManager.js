12d9e97ba826d84516193535313903f6952f9aff
Majid Ataee <ataiemajid_63@hotmail.com>
2018-03-26 01:25:38 +0430

    Created based of calendar manager.

--- src/CalendarManager.js
+++ src/CalendarManager.js
@@ -1,6 +1,57 @@
 class CalendarManager {
 	
-	constructor () {
-		
+	constructor (timestamp, timezoneOffset) {
+		this.Gregorian = new GregorianCalendar();	
+		this.Jalali = new JalaliCalendar();	
+		this.Islamic = new IslamicCalendar();	
+		this.Shia = new ShiaCalendar();
+
+		let date = new Date();
+		this._timestamp = timestamp || Math.floor(date.getTime() / 1000); // milisecond to seconds
+		this._timezoneOffset = timezoneOffset || date.getTimezoneOffset() * 60; // minute * 60 = offset in seconds
+	}
+
+	gregorian (year, month, day, hour, minute, second) {
+		this._timestamp = this.Gregorian.dateToTimestamp(year, month, day, hour, minute, second);
+
+		return this;
+	}
+
+	jalali (year, month, day, hour, minute, second) {
+		this._timestamp = this.Jalali.dateToTimestamp(year, month, day, hour, minute, second);
+
+		return this;
+	}
+
+	islamic (year, month, day, hour, minute, second) {
+		this._timestamp = this.Islamic.dateToTimestamp(year, month, day, hour, minute, second);
+
+		return this;
+	}
+
+	shia (year, month, day, hour, minute, second) {
+		this._timestamp = this.Shia.dateToTimestamp(year, month, day, hour, minute, second);
+
+		return this;
+	}
+
+	setTimestamp (timestamp) {
+		this._timestamp = timestamp;
+
+		return this;
+	}
+
+	getTimestamp () {
+		return this._timestamp;
+	}
+
+	setTimezoneOffset (timezoneOffset) {
+		this._timezoneOffset = timezoneOffset;
+
+		return this;
+	}
+
+	getTimezoneOffset () {
+		return this._timezoneOffset;
 	}
 }
