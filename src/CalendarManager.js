57db8e7ce040d88ca6d2a914277fe7f7c1d93a97
Majid Ataee <ataiemajid_63@hotmail.com>
2018-03-30 16:04:23 +0430

    Sets default current date and time in calendars methods.

--- src/CalendarManager.js
+++ src/CalendarManager.js
@@ -1,57 +1,70 @@
 class CalendarManager {
 	
 	constructor (timestamp, timezoneOffset) {
-		this.Gregorian = new GregorianCalendar();	
-		this.Jalali = new JalaliCalendar();	
-		this.Islamic = new IslamicCalendar();	
-		this.Shia = new ShiaCalendar();
+		this._gregorian = new GregorianCalendar();	
+		this._jalali = new JalaliCalendar();	
+		this._islamic = new IslamicCalendar();	
+		this._shia = new ShiaCalendar();
+		this._currentCalendar = null;
 
 		let date = new Date();
 		this._timestamp = timestamp || Math.floor(date.getTime() / 1000); // milisecond to seconds
-		this._timezoneOffset = timezoneOffset || date.getTimezoneOffset() * 60; // minute * 60 = offset in seconds
+		this._timezoneOffset = timezoneOffset || -date.getTimezoneOffset() * 60; // minute * 60 = offset in seconds
 	}
 
 	gregorian (year, month, day, hour, minute, second) {
-		this._timestamp = this.Gregorian.dateToTimestamp(year, month, day, hour, minute, second);
+		let date = this._gregorian.timestampToDate(this._timestamp);		
+		year = year || date.year;
+		month = month || date.month;
+		day = day || date.day;
+		hour = hour || date.hour;
+		minute = minute || date.minute;
+		second = second || date.second;
 
+		this._timestamp = this._gregorian.dateToTimestamp(year, month, day, hour, minute, second);
+		this._currentCalendar = this._gregorian;
 		return this;
 	}
 
 	jalali (year, month, day, hour, minute, second) {
-		this._timestamp = this.Jalali.dateToTimestamp(year, month, day, hour, minute, second);
+		let date = this._jalali.timestampToDate(this._timestamp);		
+		year = year || date.year;
+		month = month || date.month;
+		day = day || date.day;
+		hour = hour || date.hour;
+		minute = minute || date.minute;
+		second = second || date.second;
 
+		this._timestamp = this._jalali.dateToTimestamp(year, month, day, hour, minute, second);
+		this._currentCalendar = this._jalali;
 		return this;
 	}
 
 	islamic (year, month, day, hour, minute, second) {
-		this._timestamp = this.Islamic.dateToTimestamp(year, month, day, hour, minute, second);
+		let date = this._islamic.timestampToDate(this._timestamp);		
+		year = year || date.year;
+		month = month || date.month;
+		day = day || date.day;
+		hour = hour || date.hour;
+		minute = minute || date.minute;
+		second = second || date.second;
 
+		this._timestamp = this._islamic.dateToTimestamp(year, month, day, hour, minute, second);
+		this._currentCalendar = this._islamic;
 		return this;
 	}
 
 	shia (year, month, day, hour, minute, second) {
-		this._timestamp = this.Shia.dateToTimestamp(year, month, day, hour, minute, second);
+		let date = this._shia.timestampToDate(this._timestamp);		
+		year = year || date.year;
+		month = month || date.month;
+		day = day || date.day;
+		hour = hour || date.hour;
+		minute = minute || date.minute;
+		second = second || date.second;
 
+		this._timestamp = this._shia.dateToTimestamp(year, month, day, hour, minute, second);
+		this._currentCalendar = this._shia;
 		return this;
-	}
-
-	setTimestamp (timestamp) {
-		this._timestamp = timestamp;
-
-		return this;
-	}
-
-	getTimestamp () {
-		return this._timestamp;
-	}
-
-	setTimezoneOffset (timezoneOffset) {
-		this._timezoneOffset = timezoneOffset;
-
-		return this;
-	}
-
-	getTimezoneOffset () {
-		return this._timezoneOffset;
-	}
+	}	
 }
