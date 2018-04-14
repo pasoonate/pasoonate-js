7e20a4e2bb3f0288d7e1c5d220fa3e9010b8f985
Majid Ataee <ataiemajid_63@hotmail.com>
2018-04-14 11:23:52 +0430

    addMonth and subMonth methods are done. Some bugs is fixed. Build and Minify pasoonate.

--- src/mixin/AdditionAndSubstractionMixin.js
+++ src/mixin/AdditionAndSubstractionMixin.js
@@ -7,30 +7,33 @@ let AdditionAndSubstractionMixin = {
 		return this;
 	},
 
-	addMount(count) {
-		// let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
-		// let timestamp = this._currentCalendar.dateToTimestamp(date.year, date.month + count, date.day, date.hour, date.minute, date.second);
-		// this._timestamp = timestamp - this._timezoneOffset;
+	addMonth(count) {
+		let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
+		let totalMonth = date.month + count;
+		let year = date.year + Math.floor(totalMonth / 12);
+		let month = totalMonth % 12;
+		let timestamp = this._currentCalendar.dateToTimestamp(year, month, date.day, date.hour, date.minute, date.second);
+		this._timestamp = timestamp - this._timezoneOffset;
 		return this;
 	},
 
 	addDay(count) {
-		this._timestamp = timestamp + (count * 86400);
+		this._timestamp += (count * 86400);
 		return this;
 	},
 
 	addHour(count) {
-		this._timestamp = timestamp + (count * 3600);
+		this._timestamp += (count * 3600);
 		return this;
 	},
 
 	addMinute(count) {
-		this._timestamp = timestamp + (count * 60);
+		this._timestamp += (count * 60);
 		return this;
 	},
 
 	addSecond(count) {
-		this._timestamp = timestamp + count;
+		this._timestamp += count;
 		return this;
 	},
 
@@ -41,30 +44,40 @@ let AdditionAndSubstractionMixin = {
 		return this;
 	},
 
-	subMount(count) {
-		// let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
-		// let timestamp = this._currentCalendar.dateToTimestamp(date.year, date.month + count, date.day, date.hour, date.minute, date.second);
-		// this._timestamp = timestamp - this._timezoneOffset;
+	subMonth(count) {
+		let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
+		let totalMonth = date.month - count;
+		let year = date.year;
+		let month = totalMonth;
+
+		if(totalMonth <= 0) {
+			totalMonth = -totalMonth;
+			year -= Math.floor(totalMonth / 12) + 1;
+			month = 12 - (totalMonth % 12);
+		}
+
+		let timestamp = this._currentCalendar.dateToTimestamp(year, month, date.day, date.hour, date.minute, date.second);
+		this._timestamp = timestamp - this._timezoneOffset;
 		return this;
 	},
 
 	subDay(count) {
-		this._timestamp = timestamp - (count * 86400);
+		this._timestamp -= (count * 86400);
 		return this;
 	},
 
 	subHour(count) {
-		this._timestamp = timestamp - (count * 3600);
+		this._timestamp -= (count * 3600);
 		return this;
 	},
 
 	subMinute(count) {
-		this._timestamp = timestamp - (count * 60);
+		this._timestamp -= (count * 60);
 		return this;
 	},
 
 	subSecond(count) {
-		this._timestamp = timestamp - count;
+		this._timestamp -= count;
 		return this;
 	}
 };
