ea3fc2d8149ce72dbe6310f50107dd3831c18742
Majid Ataee <ataiemajid_63@hotmail.com>
2018-04-19 07:43:22 +0430

    Added localization class to pasoonate. Build and compress pasoonatejs.

--- src/Pasoonate.js
+++ src/Pasoonate.js
@@ -7,4 +7,26 @@ class Pasoonate extends Constants {
 	static make (timestamp, timezoneOffset) {
 		return new CalendarManager(timestamp, timezoneOffset);
 	}
+
+	static trans (key, locale) {
+		return Pasoonate.localization.trans(key, locale);
+	}
+
+	static setLocale (locale) {
+		return Pasoonate.localization.setLocale(locale);
+	}
+
+	static getLocal () {
+		return Pasoonate.localization.getLocal();
+	}
+
+	static isLocal (locale) {
+		return Pasoonate.localization.isLocal(locale);
+	}
 }
+
+Pasoonate.localization = new Localization();
+Object.defineProperty(Pasoonate, 'localization', {
+    writable: false,
+    configurable: false
+});
