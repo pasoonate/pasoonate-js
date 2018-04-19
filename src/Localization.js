e0ac01af61fed23b89611b99e8b38efa6aabc041
Majid Ataee <ataiemajid_63@hotmail.com>
2018-04-19 07:41:25 +0430

    Implemented localization class.

---
+++ src/Localization.js
@@ -0,0 +1,51 @@
+
+class Localization {
+
+    constructor () {
+        this._langs = {};
+        this._locale = 'fa';
+    }
+
+    setLang (name, trans) {
+        this._langs[name] = trans;
+    }
+
+    setLocale(locale) {
+        this._locale = locale || this._locale;
+    }
+
+    getLocale () {
+        return this._locale;
+    }
+
+    isLocale (locale) {
+        return this._locale === locale;
+    }
+
+    hasTransKey (key, locale) {
+        let subKeys = key.split('.');
+        if (this._langs[locale] == undefined) return false;
+        let result = this._langs[locale];
+        for (let i = 0; i < subKeys.length; i++) {
+            if (subKeys[i] in result) {
+                result = result[subKeys[i]];
+                continue;
+            }
+
+            return false;
+        }
+
+        return result;
+    }
+
+    getTrans (key, locale) {
+        let result = this.hasTransKey(key, locale);
+        return result ? result : key;
+    }
+
+    trans (key, locale) {
+        locale = locale || this._locale;
+        key = key || '';
+        return this.getTrans(key, locale);
+    }
+}
