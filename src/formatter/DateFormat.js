64b91bc882c5ec8e3d19f2d63f48521651c354d4
Majid Ataee <ataiemajid_63@hotmail.com>
2018-07-20 09:46:08 +0430

    Added simple date formatter but it has bug.

---
+++ src/formatter/DateFormat.js
@@ -0,0 +1,23 @@
+
+class DateFormat {
+	
+	constructor () {
+		this._calendar = null;
+	}
+
+	setCalendar (calendar) {
+		this._calendar = calendar instanceof CalendarManager ? calendar : null;
+	}
+
+	getCalendar () {
+		return this._calendar;
+	}
+
+	format () {
+		if(this.getCalendar() === null) {
+			return "";
+		}
+
+		return `${this._calendar.getYear()}-${this._calendar.getMonth()}-${this._calendar.getDay()} ${this._calendar.getHour()}:${this._calendar.getMinute()}:${this._calendar.getSecond()}`;
+	}
+}
---
+++ src/formatter/SimpleDateFormat.js
@@ -0,0 +1,69 @@
+
+class SimpleDateFormat extends DateFormat {
+	
+	constructor () {
+		super();
+	}
+
+	format (pattern, locale) {
+		return this.compile(pattern, locale);
+	}
+
+	compile (pattern, locale) {
+		pattern = String(pattern).toLowerCase();
+
+		let result = pattern;
+
+		const Signs = ['y', 'm', 'd'];
+		const FullYear = 'yyyy';
+		const ShortYear = 'yy';
+		const Month = 'mm';
+		const SingleMonth = 'm';
+		const Day = 'dd';
+		const SingleDay = 'd';
+
+		let chars = [];
+		let prevChar = '';
+		let currChar = '';
+		let index = 0;
+
+		for (let i = 0; i < pattern.length; i++) {
+			currChar = Signs.includes(pattern[i]) ? pattern[i] : '';
+
+			if(currChar === '') {
+				continue;
+			}
+
+			if(currChar === prevChar) {
+				chars[index].text += currChar;
+			} else {
+				chars[++index] = { text: currChar, position: i};
+			}
+			prevChar = currChar;
+		}
+console.log(chars);
+		for (let i in chars) {
+			switch (chars[i].text) {
+				case FullYear:
+					result = result.replace(FullYear, this.getCalendar().getYear());
+				break;
+				case ShortYear:
+					result = result.replace(ShortYear, String(this.getCalendar().getYear()).substr(-2, 2));
+				break;
+				case SingleMonth:
+					result = result.replace(SingleMonth, this.getCalendar().getMonth());
+				break;
+				case Month:
+					result = result.replace(Month, this.getCalendar().getMonth() > 9 ? this.getCalendar().getMonth() : '0' + this.getCalendar().getMonth());
+				break;
+				case SingleDay:
+					result = result.replace(SingleDay, this.getCalendar().getDay());
+				break;
+				case Day:
+					result = result.replace(Day, this.getCalendar().getDay() > 9 ? this.getCalendar().getDay() : '0' + this.getCalendar().getDay());
+				break;
+			}
+		}
+		return result;
+	}
+}
