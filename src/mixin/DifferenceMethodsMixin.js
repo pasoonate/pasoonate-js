a56ee240fe6c384a81c90aa545acb093be140185
Majid Ataee <ataiemajid_63@hotmail.com>
2018-11-08 19:11:09 +0330

    Added diff method but have bugs.

--- src/mixin/DifferenceMethodsMixin.js
+++ src/mixin/DifferenceMethodsMixin.js
@@ -1,9 +1,30 @@
 
 const DifferenceMethodsMixin = {
     diff (secondInstance) {
-        const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
+        let diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
 
-        return diffInSeconds;
+        const years = Math.floor(diffInSeconds / 31536000); // Constants.DaysOfJalaliYear * 24 * 60 * 60
+        diffInSeconds %= 31536000;
+        const months = Math.floor(diffInSeconds / 2592000); // 30 * 24 * 60 * 60
+        diffInSeconds %= 2592000;
+        const days = Math.floor(diffInSeconds / 86400); // 24 * 60 * 60
+        diffInSeconds %= 86400;
+        const hours = Math.floor(diffInSeconds / 3600); // 60 * 60
+        diffInSeconds %= 3600;
+        const minutes = Math.floor(diffInSeconds / 60);
+        diffInSeconds %= 60;
+        const seconds = diffInSeconds;
+
+        const diff = {
+            years: years,
+            months: months,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+
+        return diff;
     },
 
     diffInSeconds (secondInstance) {
@@ -14,35 +35,35 @@ const DifferenceMethodsMixin = {
 
     diffInMinutes (secondInstance) {
         const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
-        const diffInMinutes = diffInSeconds > 60 ? Math.ceil(diffInSeconds / 60) : 0;
+        const diffInMinutes = diffInSeconds > 60 ? Math.floor(diffInSeconds / 60) : 0;
 
         return diffInMinutes;
     },
 
     diffInHours (secondInstance) {
         const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
-        const diffInHours = diffInSeconds > 3600 ? Math.ceil(diffInSeconds / 3600) : 0;
+        const diffInHours = diffInSeconds > 3600 ? Math.floor(diffInSeconds / 3600) : 0;
 
         return diffInHours;
     },
 
     diffInDays (secondInstance) {
         const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
-        const diffInDays = diffInSeconds > 86400 ? Math.ceil(diffInSeconds / 86400) : 0;
+        const diffInDays = diffInSeconds > 86400 ? Math.floor(diffInSeconds / 86400) : 0;
 
         return diffInDays;
     },
 
     diffInMonths (secondInstance) {
         const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
-        const diffInMonths = diffInSeconds > 2592000 ? Math.ceil(diffInSeconds / 2592000) : 0;
+        const diffInMonths = diffInSeconds > 2592000 ? Math.floor(diffInSeconds / 2592000) : 0;
 
         return diffInMonths;
     },
 
     diffInYears (secondInstance) {
         const diffInSeconds = Math.abs(this.getTimestamp() - secondInstance.getTimestamp());
-        const diffInYears = diffInSeconds > 31104000 ? Math.ceil(diffInSeconds / 31104000) : 0;
+        const diffInYears = diffInSeconds > 31536000 ? Math.floor(diffInSeconds / 31536000) : 0;
 
         return diffInYears;
     }
