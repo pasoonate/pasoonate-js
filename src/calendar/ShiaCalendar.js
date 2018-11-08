4e5d9ac7e6681ae50a3f3507e475284e714348b7
Majid Ataee <ataiemajid_63@hotmail.com>
2018-11-08 18:45:41 +0330

    Bug fixed in shai calendar and compile them.

--- src/calendar/ShiaCalendar.js
+++ src/calendar/ShiaCalendar.js
@@ -12,40 +12,48 @@ class ShiaCalendar extends Calendar {
 	}
 
 	dateToJulianDay (year, month, day, hour, minute, second) {
+        const daysInMonth = this.daysInMonth(year, month);
         let dayOfYear = day;
         let julianDay = 0;
 
-        for (let i = 1; i < month; i++) {
-            dayOfYear += this.daysInMonth(year, i);
+        if(day > daysInMonth) {
+            dayOfYear = day - daysInMonth;
+            year = month === 12 ? year + 1 : year;
+            month = month === 12 ? 1 : month + 1;
+        }
+
+        for (let m = 1; m < month; m++) {
+            dayOfYear += this.daysInMonth(year, m);
         }
 
         julianDay += dayOfYear;
-        julianDay += (year - 1) * 354;
+        julianDay += (year - 1) * Constants.DaysOfShiaYear;
         julianDay += Math.floor(((11 * year) + 3) / 30);
-        julianDay += this.ShiaEpoch - 1;
-
+        julianDay += this.ShiaEpoch - (year === 1440 ? 2 : 1);
 		return this.addTimeToJulianDay(julianDay, hour, minute, second);
 	}
 
     julianDayToDate (julianDay) {
         let time = this.extractJulianDayTime(julianDay);
-        
+            
         julianDay = this.julianDayWithoutTime(julianDay);
-        julianDay = Math.floor(julianDay) + 0.5;
-        
+
         let year = Math.floor((((julianDay - this.ShiaEpoch) * 30) + 10646) / 10631);
         let month = Math.min(12, Math.ceil((julianDay - (29 + this.julianDayWithoutTime(this.dateToJulianDay(year, 1, 1, time.hour, time.minute, time.second)))) / 29.5) + 1);
-        let dayOfYear = julianDay - this.julianDayWithoutTime(this.dateToJulianDay(year, 1, 1, time.hour, time.minute, time.second));
+        let dayOfYear = julianDay - this.julianDayWithoutTime(this.dateToJulianDay(year - 1, 12, this.daysInMonth(year - 1, 12), time.hour, time.minute, time.second));
         let days = 0;
+
         for (let i = 1; i <= 12; i++) {
             days += this.daysInMonth(year, i);
-            if (dayOfYear < days) {
+            if (dayOfYear <= days) {
                 month = i;
                 break;
             }
         }
-        let day = (julianDay - ((days - this.daysInMonth(year, month)) + ((year - 1) * 354) + Math.floor((3 + (11 * year)) / 30) + this.ShiaEpoch) + 1);
 
+        // let day = (julianDay - ((days - this.daysInMonth(year, month)) + ((year - 1) * 354) + Math.floor((3 + (11 * year)) / 30) + this.ShiaEpoch) + 1);
+        let day = dayOfYear - (days - this.daysInMonth(year, month));
+        
         return {
         	"year": year,
         	"month": month,
