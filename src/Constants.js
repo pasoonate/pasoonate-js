c87d974d8ac219d3c338f6206347eab4ba2b33de
Majid Ataee <ataiemajid_63@hotmail.com>
2018-11-08 18:43:28 +0330

    Added some const to constants class.

--- src/Constants.js
+++ src/Constants.js
@@ -5,3 +5,14 @@ class Constants {
 
 	}
 }
+
+Constants.J1970 = 2440587.5; // Julian date at Unix epoch: 1970-01-01
+Constants.DayInSecond = 86400;
+Constants.ShiaEpoch = 1948439.5;
+Constants.JalaliEpoch = 1948320.5;
+Constants.GregorianEpoch = 1721425.5;
+Constants.IslamicEpoch = 1948439.5;
+Constants.DaysOfIslamicYear = 354;
+Constants.DaysOfShiaYear = 354;
+Constants.DaysOfJalaliYear = 365;
+Constants.DaysOfGaregorianYear = 365;
