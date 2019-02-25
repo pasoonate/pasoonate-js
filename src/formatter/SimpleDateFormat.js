bc5a9ef291e72b7d89851163bac730bff74bfd87
Majid Ataee <ataiemajid_63@hotmail.com>
2019-02-25 17:20:02 +0330

    Fixed and update format pattern.

--- src/formatter/SimpleDateFormat.js
+++ src/formatter/SimpleDateFormat.js
@@ -1,4 +1,5 @@
 import DateFormat from "./DateFormat";
+import Pasoonate from "../Pasoonate";
 
 class SimpleDateFormat extends DateFormat {
 	
@@ -11,61 +12,100 @@ class SimpleDateFormat extends DateFormat {
 	}
 
 	compile (pattern, locale) {
-		pattern = String(pattern).toLowerCase();
+		pattern = String(pattern);
 
-		let result = pattern;
-
-		const Signs = ['y', 'm', 'd'];
 		const FullYear = 'yyyy';
 		const ShortYear = 'yy';
-		const Month = 'mm';
-		const SingleMonth = 'm';
-		const Day = 'dd';
-		const SingleDay = 'd';
+		const FullMonthName = 'MMMM';
+		const ShortMonthName = 'MMM';
+		const FullMonth = 'MM';
+		const ShortMonth = 'M';
+		const ShortDayName = 'ddd';
+		const FullDayName = 'dddd';
+		const FullDay = 'dd';
+		const ShortDay = 'd';
+		const FullHour = 'HH';
+		const ShortHour = 'H';
+		const FullMinute = 'mm';
+		const ShortMinute = 'm';
+		const FullSecond = 'ss';
+		const ShortSecond = 's';
 
-		let chars = [];
+		let categories = [];
 		let prevChar = '';
 		let currChar = '';
 		let index = 0;
 
 		for (let i = 0; i < pattern.length; i++) {
-			currChar = Signs.includes(pattern[i]) ? pattern[i] : '';
+			currChar = pattern[i];
 
 			if(currChar === '') {
 				continue;
 			}
 
 			if(currChar === prevChar) {
-				chars[index].text += currChar;
+				categories[index] += currChar;
 			} else {
-				chars[++index] = { text: currChar, position: i};
+				categories[++index] = currChar;
 			}
+
 			prevChar = currChar;
 		}
 
-		for (let i in chars) {
-			switch (chars[i].text) {
+		for (let i in categories) {
+			switch (categories[i]) {
 				case FullYear:
-					result = result.replace(FullYear, this.getCalendar().getYear());
+					categories[i] = this.getCalendar().getYear();
 				break;
 				case ShortYear:
-					result = result.replace(ShortYear, String(this.getCalendar().getYear()).substr(-2, 2));
+					categories[i] = String(this.getCalendar().getYear()).substr(-2, 2);
 				break;
-				case SingleMonth:
-					result = result.replace(SingleMonth, this.getCalendar().getMonth());
+				case FullMonthName:
+					categories[i] = Pasoonate.trans(`${this.getCalendar().name()}.month_name.${this.getCalendar().getMonth()}`);
 				break;
-				case Month:
-					result = result.replace(Month, this.getCalendar().getMonth() > 9 ? this.getCalendar().getMonth() : '0' + this.getCalendar().getMonth());
+				case ShortMonthName:
+					categories[i] = Pasoonate.trans(`${this.getCalendar().name()}.short_month_name.${this.getCalendar().getMonth()}`);
 				break;
-				case SingleDay:
-					result = result.replace(SingleDay, this.getCalendar().getDay());
+				case FullMonth:
+					categories[i] = this.getCalendar().getMonth() > 9 ? this.getCalendar().getMonth() : `0${this.getCalendar().getMonth()}`;
 				break;
-				case Day:
-					result = result.replace(Day, this.getCalendar().getDay() > 9 ? this.getCalendar().getDay() : '0' + this.getCalendar().getDay());
+				case ShortMonth:
+					categories[i] = this.getCalendar().getMonth();
+				break;
+				case FullDayName:
+					categories[i] = Pasoonate.trans(`${this.getCalendar().name()}.short_day_name.${this.getCalendar().getDay()}`);
+				break;
+				case ShortDayName:
+					categories[i] = Pasoonate.trans(`${this.getCalendar().name()}.day_name.${this.getCalendar().getDay()}`);
+				break;
+				case FullDay:
+					categories[i] = this.getCalendar().getDay() > 9 ? this.getCalendar().getDay() : `0${this.getCalendar().getDay()}`;
+				break;
+				case ShortDay:
+					categories[i] = this.getCalendar().getDay();
+				break;
+				case FullHour:
+					categories[i] = this.getCalendar().getHour() > 9 ? this.getCalendar().getHour() : `0${this.getCalendar().getHour()}`;
+				break;
+				case ShortHour:
+					categories[i] = this.getCalendar().getHour();
+				break;
+				case FullMinute:
+					categories[i] = this.getCalendar().getMinute() > 9 ? this.getCalendar().getMinute() : `0${this.getCalendar().getMinute()}`;
+				break;
+				case ShortMinute:
+					categories[i] = this.getCalendar().getMinute();
+				break;
+				case FullSecond:
+					categories[i] = this.getCalendar().getSecond() > 9 ? this.getCalendar().getSecond() : `0${this.getCalendar().getSecond()}`;
+				break;
+				case ShortSecond:
+					categories[i] = this.getCalendar().getSecond();
 				break;
 			}
 		}
-		return result;
+
+		return categories.join('');
 	}
 }
 
