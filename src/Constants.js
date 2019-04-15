da5a39beb2aae8e8ef504cb5fc725e799bf74483
Majid Ataee <ataiemajid_63@hotmail.com>
2019-04-15 19:33:07 +0430

    Append new const value to the Constants object.

--- src/Constants.js
+++ src/Constants.js
@@ -1,19 +1,29 @@
-class Constants {
-	 
-	constructor () {
-
-	}
-}
-
-Constants.J1970 = 2440587.5; // Julian date at Unix epoch: 1970-01-01
-Constants.DayInSecond = 86400;
-Constants.ShiaEpoch = 1948439.5;
-Constants.JalaliEpoch = 1948320.5;
-Constants.GregorianEpoch = 1721425.5;
-Constants.IslamicEpoch = 1948439.5;
-Constants.DaysOfIslamicYear = 354;
-Constants.DaysOfShiaYear = 354;
-Constants.DaysOfJalaliYear = 365;
-Constants.DaysOfGregorianYear = 365;
+const Constants = {
+	J1970: 2440587.5, // Julian date at Unix epoch: 1970-01-01
+	SATURDAY: 1,
+	Sunday: 2,
+	Monday: 3,
+	Tuesday: 4,
+	Wednesday: 5,
+	Thursday: 6,
+	Friday: 7,
+	YearsPerCentury: 100,
+	YearsPerDecade: 10,
+	MonthsPerYear: 12,
+	WeeksPerYear: 52,
+	DaysPerWeek: 7,
+	HoursPerDay: 24,
+	MinutesPerHour: 60,
+	SecondsPerMinute: 60,
+	DayInSecond: 86400,
+	ShiaEpoch: 1948439.5,
+	JalaliEpoch: 1948320.5,
+	GregorianEpoch: 1721425.5,
+	IslamicEpoch: 1948439.5,
+	DaysOfIslamicYear: 354,
+	DaysOfShiaYear: 354,
+	DaysOfJalaliYear: 365,
+	DaysOfGregorianYear: 365,
+};
 
 export default Constants;
