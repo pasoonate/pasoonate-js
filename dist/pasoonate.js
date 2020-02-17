4297b95b8aab981dfd29ed12188caddd1d0d8938
Majid Ataee <ataiemajid_63@hotmail.com>
2020-02-17 12:43:17 +0330

    Fixed subMonth, addMonth, subYear and addYear methods.

--- dist/pasoonate.js
+++ dist/pasoonate.js
@@ -1,8 +1,12 @@
 const AdditionAndSubtractionMixin = {
 	addYear(count) {
 		let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
-		let timestamp = this._currentCalendar.dateToTimestamp(date.year + count, date.month, date.day, date.hour, date.minute, date.second);
+		let year = date.year + count;
+		let daysInMonth = this._currentCalendar.daysInMonth(year, date.month);
+		let day = date.day <= daysInMonth ? date.day : daysInMonth;
+		let timestamp = this._currentCalendar.dateToTimestamp(year, date.month, day, date.hour, date.minute, date.second);
 		this._timestamp = timestamp - this._timezoneOffset;
+
 		return this;
 	},
 
@@ -11,35 +15,47 @@ const AdditionAndSubtractionMixin = {
 		let totalMonth = date.month + count;
 		let year = date.year + Math.ceil((totalMonth / 12) - 1);
 		let month = (totalMonth % 12) === 0 ? 12 : (totalMonth % 12);
-		let timestamp = this._currentCalendar.dateToTimestamp(year, month, date.day, date.hour, date.minute, date.second);
+		let daysInMonth = this._currentCalendar.daysInMonth(year, month);
+		let day = date.day <= daysInMonth ? date.day : daysInMonth;
+		let timestamp = this._currentCalendar.dateToTimestamp(year, month, day, date.hour, date.minute, date.second);
+
 		this._timestamp = timestamp - this._timezoneOffset;
+
 		return this;
 	},
 
 	addDay(count) {
 		this._timestamp += (count * 86400);
+
 		return this;
 	},
 
 	addHour(count) {
 		this._timestamp += (count * 3600);
+
 		return this;
 	},
 
 	addMinute(count) {
 		this._timestamp += (count * 60);
+
 		return this;
 	},
 
 	addSecond(count) {
 		this._timestamp += count;
+
 		return this;
 	},
 
 	subYear(count) {
 		let date = this._currentCalendar.timestampToDate(this._timestamp + this._timezoneOffset);
-		let timestamp = this._currentCalendar.dateToTimestamp(date.year - count, date.month, date.day, date.hour, date.minute, date.second);
+		let year = date.year - count;
+		let daysInMonth = this._currentCalendar.daysInMonth(year, date.month);
+		let day = date.day <= daysInMonth ? date.day : daysInMonth;
+		let timestamp = this._currentCalendar.dateToTimestamp(year, date.month, day, date.hour, date.minute, date.second);
 		this._timestamp = timestamp - this._timezoneOffset;
+
 		return this;
 	},
 
@@ -55,28 +71,35 @@ const AdditionAndSubtractionMixin = {
 			month = 12 - (totalMonth % 12);
 		}
 
-		let timestamp = this._currentCalendar.dateToTimestamp(year, month, date.day, date.hour, date.minute, date.second);
+		let daysInMonth = this._currentCalendar.daysInMonth(year, month);
+		let day = date.day <= daysInMonth ? date.day : daysInMonth;
+		let timestamp = this._currentCalendar.dateToTimestamp(year, month, day, date.hour, date.minute, date.second);
 		this._timestamp = timestamp - this._timezoneOffset;
+
 		return this;
 	},
 
 	subDay(count) {
 		this._timestamp -= (count * 86400);
+
 		return this;
 	},
 
 	subHour(count) {
 		this._timestamp -= (count * 3600);
+
 		return this;
 	},
 
 	subMinute(count) {
 		this._timestamp -= (count * 60);
+
 		return this;
 	},
 
 	subSecond(count) {
 		this._timestamp -= count;
+
 		return this;
 	}
 };
