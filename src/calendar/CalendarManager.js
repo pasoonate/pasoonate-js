6fafb19de9ead4adadb83c3182bd919fea9e52fb
Majid Ataee <ataiemajid_63@hotmail.com>
2022-07-03 07:22:55 +0430

    Fixed set timezone offset in constructor.

--- src/calendar/CalendarManager.js
+++ src/calendar/CalendarManager.js
@@ -21,7 +21,7 @@ class CalendarManager {
 
 		let date = new Date();
 		this._timestamp = timestamp || (Math.floor(date.getTime() / 1000)); // millisecond to seconds
-		this._timezoneOffset = timezoneOffset !== undefined || -date.getTimezoneOffset() * 60; // minute * 60 = offset in seconds
+		this._timezoneOffset = timezoneOffset || -date.getTimezoneOffset() * 60; // minute * 60 = offset in seconds
 	}
 
 	gregorian (dateTime) {
